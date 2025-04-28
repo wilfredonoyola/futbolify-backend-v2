9b83c298dc6022e0104f808291184854c3940a7a
wn.masternetworks <wn.masternetworks@gmail.com>
2025-04-28 08:43:38 -0600

    chore fix enable origin!

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,28 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
+import { Logger, ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
 
-const logger = new Logger('App');
+const logger = new Logger('App')
 
 async function bootstrap() {
-  Logger.overrideLogger(['log', 'error', 'warn', 'debug', 'verbose']);
+  Logger.overrideLogger(['log', 'error', 'warn', 'debug', 'verbose'])
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
 
   // Configuring ValidationPipe to improve performance
   app.useGlobalPipes(
     new ValidationPipe({
       // whitelist: true,
       transform: true,
-    }),
-  );
+    })
+  )
 
-  app.useLogger(logger); // Set the logger for the app
+  app.useLogger(logger) // Set the logger for the app
 
-  const configService = app.get(ConfigService);
+  const configService = app.get(ConfigService)
 
-  const nodeEnv = configService.get<string>('NODE_ENV');
+  const nodeEnv = configService.get<string>('NODE_ENV')
 
   // Enable CORS for development, staging, and production with secure configuration
   if (
@@ -31,18 +31,19 @@ async function bootstrap() {
     nodeEnv === 'production'
   ) {
     app.enableCors({
-      origin:
-        nodeEnv === 'production' ? 'https://ng-client-rosy.vercel.app' : '*',
+      origin: '*',
+      // origin:
+      //   nodeEnv === 'production' ? 'https://ng-client-rosy.vercel.app' : '*',
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       credentials: true,
-    });
+    })
   }
 
   // Port to listen to the application
-  const port = configService.get<number>('PORT') || 3001;
-  await app.listen(port);
+  const port = configService.get<number>('PORT') || 3001
+  await app.listen(port)
 
-  logger.log(`Server run in: http://localhost:${port}/graphql`);
+  logger.log(`Server run in: http://localhost:${port}/graphql`)
 }
 
-bootstrap();
+bootstrap()
